5ded8c80d24e25f2a7878047cfe2b41d5a5d4467
Bernie <bernieliu2@gmail.com>
2025-08-12 03:41:14 -0400

    update

--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -17,7 +17,7 @@ const fetcher = async (url) =>{
 }
 const Comments = ({postSlug}) => {
     const {status} = useSession();
-    const {data, mutate, isLoading} = useSWR(`http://localhost:3000/api/comments?postSlug=${postSlug}`, fetcher)
+    const {data, mutate, isLoading} = useSWR(`https://blog.vercel.app/api/comments?postSlug=${postSlug}`, fetcher)
     const [desc,setDesc] = useState("")
 
     const handSubmit = async ()=>{
