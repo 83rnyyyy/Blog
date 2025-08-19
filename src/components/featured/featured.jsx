8906894ac5bda73af36439f38ff80f8c12cff4aa
Bernie <bernieliu2@gmail.com>
2025-08-19 05:11:15 -0400

    featured update

--- src/components/featured/featured.jsx
+++ src/components/featured/featured.jsx
@@ -10,13 +10,13 @@ const Featured = () => {
             </h1>
             <div className={styles.post}>
                 <div className={styles.imgContainer}>
-                    <Image src="/p1.jpeg" alt="Featured" fill priority sizes="(max-width: 768px) 100vw, 600px" className={styles.image} />
+                    <Image src="/featured.jpeg" alt="Featured" fill priority sizes="(max-width: 768px) 100vw, 600px" className={styles.image} />
                 </div>
 
                 <div className={styles.textContainer}>
-                    <h1 className={styles.postTitle}> asidojhaowidjaowdijasjdoaiwjd</h1>
+                    <h1 className={styles.postTitle}> TeenageTheory: Your Voice, Your Story</h1>
                     <p className={styles.postDesc}>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        A blog hub where teens can write, read, and connect through real experiences and fresh perspectives.
                     </p>
                     <button className={styles.button}>Read More</button>
                 </div>
