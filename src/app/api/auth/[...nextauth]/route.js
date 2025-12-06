aa9021e3efff794169193b3823fefb51c4a78712
Bernie <bernieliu2@gmail.com>
2025-12-06 12:07:45 -0500

    revert

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,24 +1,6 @@
+import { authOptions } from "@/utils/auth"
 import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
-import GoogleProvider from "next-auth/providers/google"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import prisma from "@/utils/connect"
 
-export const { handlers, auth, signIn, signOut } = NextAuth({
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-    }),
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    })
-  ],
-  // ADD THIS:
-  basePath: "/api/auth",
-  trustHost: true, // Important for Next.js 15+
-})
+const handler = NextAuth(authOptions)
 
-export const { GET, POST } = handlers
+export {handler as GET, handler as POST}
