774f052c19a7c2b83334cb36f22e08a2a1bcbfd3
Bernie <bernieliu2@gmail.com>
2025-12-06 12:10:25 -0500

    revert

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,21 +1,3 @@
-import GithubProvider from "next-auth/providers/github"
-import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import prisma from "./connect";
-import { getServerSession } from "next-auth";
+import { auth } from "@/app/api/auth/[...nextauth]/route"
 
-export const authOptions = {
-    adapter: PrismaAdapter(prisma),
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_ID,
-            clientSecret: process.env.GOOGLE_SECRET,
-        }),
-        GithubProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET,
-        })
-    ],
-}
-
-export const getAuthSession =()=>getServerSession(authOptions)
+export const getAuthSession = () => auth()
