8a22c8f883da00a53a15a8863a8993b46c8cde54
Bernie <bernieliu2@gmail.com>
2025-12-05 23:15:17 -0500

    fix

--- src/components/featured/featured.jsx
+++ src/components/featured/featured.jsx
@@ -10,13 +10,13 @@ const Featured = () => {
             </h1>
             <div className={styles.post}>
                 <div className={styles.imgContainer}>
-                    <Image src="/featured.jpg" alt="Featured" fill priority sizes="(max-width: 768px) 100vw, 600px" className={styles.image} />
+                    <Image src="/featured.png" alt="Featured" fill priority sizes="(max-width: 768px) 100vw, 600px" className={styles.image} />
                 </div>
 
                 <div className={styles.textContainer}>
                     <h1 className={styles.postTitle}> TeenageTheory: Your Voice, Your Story</h1>
                     <p className={styles.postDesc}>
-                        A blog hub where teens can write, read, and connect through real experiences and fresh perspectives.
+                        A blog for teenagers to read and connect through real experiences and perspectives
                     </p>
                     <button className={styles.button}>Read More</button>
                 </div>
